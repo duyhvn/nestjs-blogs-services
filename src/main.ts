15df4a7edea68220b051c14ec59daf4aca8efe94
Duy Huynh <hvnduy1504@gmail.com>
2023-07-12 16:36:39 +0700

    CORS Config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule, {
+        cors: {
+            origin: process.env.CORS_ALLOW,
+            methods: process.env.CORS_METHODS,
+            maxAge: parseInt(process.env.CORS_MAX_AGE),
+        } as CorsOptions
+    });
+    await app.listen(process.env.APP_PORT || 3000);
 }
+
 bootstrap();
